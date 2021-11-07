6213113914469dd79f73a8345f694cc55432d3cd
[Matanel1995] <[Matanel5555@gmail.com]>
2021-11-07 19:55:19 +0300

    pls work

--- main.c
+++ main.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 #include "NumClass.h"
-#define True 1
-#define False 0
-
-
-int main(void){
+int main(){
     int a , b;
     scanf("%d", &a);
     scanf("%d", &b);
-    printf("The Armstrong numbers are:");
+    printf("The Armstrong numbers are");
     for (int i = a; i <= b; i++)
     {
-        if(isArmstrong(i) == True)
+        if(isArmstrong(i) == 1)
             printf(" %d" ,i);
     }
     printf("\nThe Palindromes numbers are:");
     for (int i = a; i <= b; i++)
     {
-        if(isPalindrome(i) == True)
+        if(isPalindrome(i) == 1)
             printf(" %d" ,i);
     }
     printf("\nThe Prime numbers are:");
     for (int i = a; i <= b; i++)
     {
-        if(isPrime(i) == True)
+        if(isPrime(i) == 1)
             printf(" %d" ,i);
     }
     printf("\nThe Strong numbers are:");
     for (int i = a; i <= b; i++)
     {
-        if(isStrong(i) == True)
+        if(isStrong(i) == 1)
             printf(" %d" ,i);
     }
     return 0;
